65421a31b2f628f73db5b676c92413121147f2d6
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-03-04 09:09:18 +0530

    Enable form data parsing with urlencoded middleware

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const indexRouter = require('./routes/index')
 app.set('views',path.join(__dirname,'views','pages'));
 app.set('view engine', 'ejs')
 
+app.use(express.urlencoded({ extended: true }));
 app.use('/',indexRouter);
 
 
