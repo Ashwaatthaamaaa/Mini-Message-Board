c3d1e22cd0c5cce9f7afafd0a72c965955436f48
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-03-04 08:22:20 +0530

    Add index router to Express app

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const indexRouter = require('./routes/index')
 
 
 app.set('views',path.join(__dirname,'views','pages'));
 app.set('view engine', 'ejs')
+
+app.use('/',indexRouter);
 
 
 
