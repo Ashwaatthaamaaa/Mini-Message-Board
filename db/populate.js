3f222f160908af04673ee4cbeffbb519048e03f7
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-03-24 03:35:06 +0530

    Rename "user" column to "username" in messages table and update corresponding insert statement for consistency.

--- db/populate.js
+++ db/populate.js
@@ -5,11 +5,11 @@ const query = `
     CREATE TABLE IF NOT EXISTS messages(
         id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         text VARCHAR(3000),
-        "user" VARCHAR(255),
+        username VARCHAR(255),
         added TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
 
-    INSERT INTO messages (text, "user")
+    INSERT INTO messages (text, username)
     VALUES
     ('Hi there!', 'Amando'),
     ('Hello World!', 'Charles');
@@ -18,7 +18,6 @@ const query = `
 async function main() {
     try {
         await pool.query(query);
-        console.log('db populated');
     } catch(err) {
         console.error('error populating', err);
     } finally {
