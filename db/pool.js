d03dac8651b9532851185d5c5aa011388a10e919
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-03-24 04:29:36 +0530

    Fix database connection string in pool.js to correct the database name for proper functionality.

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: "postgresql://lunge:cmqgDT7Y8fbjrF9tojUl1fX0dGsUfNa6@dpg-cvfgillsvqrc73d1dif0-a.singapore-postgres.render.com/message_pumz",
+    connectionString: "postgresql://lunge:cmqgDT7Y8fbjrF9tojUl1fX0dGsUfNa6@dpg-cvfgillsvqrc73d1dif0-a/message_pumz",
     ssl: {
         rejectUnauthorized: false
     }
