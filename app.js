ac537bb201388158499d4f074fb57904165ee173
Ashwaatthaamaaa <sarthakk145@gmail.com>
2025-03-24 04:14:14 +0530

    Refactor app.js to improve code consistency: unify quotes, add static file serving for CSS, and streamline server setup.

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const indexRouter = require('./routes/index')
+const express = require("express")
+const app = express()
+const path = require("path")
+const indexRouter = require("./routes/index")
 
+app.set("views", path.join(__dirname, "views", "pages"))
+app.set("view engine", "ejs")
 
-app.set('views',path.join(__dirname,'views','pages'));
-app.set('view engine', 'ejs')
+// Add static file serving for CSS
+app.use(express.static(path.join(__dirname, "public")))
+app.use(express.urlencoded({ extended: true }))
+app.use("/", indexRouter)
 
-app.use(express.urlencoded({ extended: true }));
-app.use('/',indexRouter);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000")
+})
 
-
-
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
-}) 
